863fc10cac366e3a06fba03665256137742bc2b4
LuisAlejandroVega <LuisAlejandroVega@redesprivadasvirtuales.com>
2026-03-30 12:59:09 -0300

    Update 2. build.bat

--- 2. build.bat
+++ 2. build.bat
@@ -1,6 +1,21 @@
-pyinstaller --onefile --noconsole --distpath . --collect-all win10toast --name CopyLinktoClipboard copylinktoclipboard.py
+@echo off
+echo Closing processes...
+taskkill /f /im CopyLinktoClipboard.exe >nul 2>&1
+
+echo Deleting old files...
+del /q CopyLinktoClipboard.exe >nul 2>&1
+rmdir /s /q build >nul 2>&1
+del /q CopyLinktoClipboard.spec >nul 2>&1
+
+echo Compiling new version (clean)...
+pyinstaller --onefile --noconsole --clean ^
+ --collect-all win11toast ^
+ --distpath . ^
+ --name CopyLinktoClipboard copylinktoclipboard.py
+
+echo Cleaning temporaries...
 rmdir /s /q build
 del /q CopyLinktoClipboard.spec
 
-echo done.
+echo Finished!
 pause
